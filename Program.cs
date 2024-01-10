d56290eb8a8832bd5fa2fec6043669a3f2d02c20
Pekka Alaluukas <pekka.alaluukas@oamk.fi>
2024-01-10 18:05:40 +0200

    ehtorakenne toimii

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//ehtorakenne
+int number;
+Console.WriteLine("Anna luku väliltä 0-10");
+number = int.Parse(Console.ReadLine());
+if(number < 0){
+    Console.WriteLine("Annoit liian pienen luvun");
+}
+else if (number > 10){
+    Console.WriteLine("Luku on liian suuri");
+}
+else {
+    Console.WriteLine("Luku on tarpeeksi suuri");
+}
+Console.WriteLine("LOPPU 1!");
+
+//sama toisella tavalla
+if(number < 0){
+    Console.WriteLine("2.Annoit liian pienen luvun");
+}
+else if (number <= 10){
+    Console.WriteLine("2.Luku on tarpeeksi suuri");
+}
+else {
+    Console.WriteLine("2.Luku on liian suuri");
+}
+Console.WriteLine("LOPPU 2!");
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # console_harj1
+
+Tässä harjoituksessa tutkittiin **if else** rakennetta.
